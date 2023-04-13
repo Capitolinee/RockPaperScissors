9a5f7d89eaf0d1351e37cc34eb72ffe753b31f4a
Capitoline <62502450+Capitolinee@users.noreply.github.com>
2023-04-13 17:01:53 +0800

    Add files via upload

---
+++ RockPaperScissors/RockPaperScissors/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿
+namespace RockPaperScissors
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbReceivePort = new System.Windows.Forms.TextBox();
+            this.tbTargetIP = new System.Windows.Forms.TextBox();
+            this.tbTargetPort = new System.Windows.Forms.TextBox();
+            this.btnActivate = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.radScissors = new System.Windows.Forms.RadioButton();
+            this.radStone = new System.Windows.Forms.RadioButton();
+            this.radPaper = new System.Windows.Forms.RadioButton();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(88, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "接收Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(86, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "目標IP";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(383, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "目標Port";
+            // 
+            // tbReceivePort
+            // 
+            this.tbReceivePort.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tbReceivePort.Location = new System.Drawing.Point(206, 28);
+            this.tbReceivePort.Name = "tbReceivePort";
+            this.tbReceivePort.Size = new System.Drawing.Size(136, 35);
+            this.tbReceivePort.TabIndex = 7;
+            // 
+            // tbTargetIP
+            // 
+            this.tbTargetIP.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tbTargetIP.Location = new System.Drawing.Point(206, 87);
+            this.tbTargetIP.Name = "tbTargetIP";
+            this.tbTargetIP.Size = new System.Drawing.Size(136, 35);
+            this.tbTargetIP.TabIndex = 8;
+            // 
+            // tbTargetPort
+            // 
+            this.tbTargetPort.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tbTargetPort.Location = new System.Drawing.Point(501, 81);
+            this.tbTargetPort.Name = "tbTargetPort";
+            this.tbTargetPort.Size = new System.Drawing.Size(126, 35);
+            this.tbTargetPort.TabIndex = 9;
+            // 
+            // btnActivate
+            // 
+            this.btnActivate.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnActivate.Location = new System.Drawing.Point(388, 28);
+            this.btnActivate.Name = "btnActivate";
+            this.btnActivate.Size = new System.Drawing.Size(148, 40);
+            this.btnActivate.TabIndex = 12;
+            this.btnActivate.Text = "啟動監聽";
+            this.btnActivate.UseVisualStyleBackColor = true;
+            this.btnActivate.Click += new System.EventHandler(this.btnActivate_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnSend.Location = new System.Drawing.Point(517, 176);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(97, 40);
+            this.btnSend.TabIndex = 13;
+            this.btnSend.Text = "傳送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // radScissors
+            // 
+            this.radScissors.AutoSize = true;
+            this.radScissors.Checked = true;
+            this.radScissors.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.radScissors.Location = new System.Drawing.Point(111, 176);
+            this.radScissors.Name = "radScissors";
+            this.radScissors.Size = new System.Drawing.Size(77, 27);
+            this.radScissors.TabIndex = 14;
+            this.radScissors.TabStop = true;
+            this.radScissors.Text = "剪刀";
+            this.radScissors.UseVisualStyleBackColor = true;
+            // 
+            // radStone
+            // 
+            this.radStone.AutoSize = true;
+            this.radStone.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.radStone.Location = new System.Drawing.Point(253, 176);
+            this.radStone.Name = "radStone";
+            this.radStone.Size = new System.Drawing.Size(77, 27);
+            this.radStone.TabIndex = 15;
+            this.radStone.Text = "叔投";
+            this.radStone.UseVisualStyleBackColor = true;
+            // 
+            // radPaper
+            // 
+            this.radPaper.AutoSize = true;
+            this.radPaper.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.radPaper.Location = new System.Drawing.Point(388, 176);
+            this.radPaper.Name = "radPaper";
+            this.radPaper.Size = new System.Drawing.Size(54, 27);
+            this.radPaper.TabIndex = 16;
+            this.radPaper.Text = "布";
+            this.radPaper.UseVisualStyleBackColor = true;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblResult.Location = new System.Drawing.Point(187, 285);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(21, 23);
+            this.lblResult.TabIndex = 17;
+            this.lblResult.Text = "_";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(88, 285);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 23);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "結果：";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Enabled = false;
+            this.btnReset.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnReset.Location = new System.Drawing.Point(517, 277);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(97, 39);
+            this.btnReset.TabIndex = 19;
+            this.btnReset.Text = "重製";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 423);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.radPaper);
+            this.Controls.Add(this.radStone);
+            this.Controls.Add(this.radScissors);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnActivate);
+            this.Controls.Add(this.tbTargetPort);
+            this.Controls.Add(this.tbTargetIP);
+            this.Controls.Add(this.tbReceivePort);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbReceivePort;
+        private System.Windows.Forms.TextBox tbTargetIP;
+        private System.Windows.Forms.TextBox tbTargetPort;
+        private System.Windows.Forms.Button btnActivate;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.RadioButton radScissors;
+        private System.Windows.Forms.RadioButton radStone;
+        private System.Windows.Forms.RadioButton radPaper;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ RockPaperScissors/RockPaperScissors/Form1.cs
@@ -0,0 +1,307 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RockPaperScissors
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // UDP通訊物件
+        UdpClient U;
+
+        // 監聽專用的執行緒
+        Thread Th;
+
+        // 對方資訊 (Opponent Pick)
+        string oppPick;
+
+        // 我方資訊
+        string myPick;
+
+        // 遊戲成果
+        string gameResult;
+
+        /// <summary>
+        /// 監聽 UDP
+        /// </summary>
+        private void Listen()
+        {
+            //取得監聽用的通訊port
+            int Port = int.Parse(tbReceivePort.Text);
+
+            //建立UDP監聽器物件
+            U = new UdpClient(Port); 
+
+            //建立本機端
+            IPEndPoint EP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), Port);
+
+            while (true)    
+            {
+                var packet = U.Receive(ref EP);
+                
+                string str = Encoding.UTF8.GetString(packet, 0, packet.Length);
+
+                if (str.ToLower().Contains("opppick"))
+                {
+                    oppPick = str.Split(':')[1];
+                }
+                else if (str.ToLower().Contains("result"))
+                {
+                    gameResult = str.Split(':')[1];
+
+                    if (gameResult == "你贏了")
+                    {
+                        lblResult.Text = "你輸了";
+                    }
+                    else if(gameResult == "你輸了")
+                    {
+                        lblResult.Text = "你贏了";
+                    }
+                    else
+                    {
+                        lblResult.Text = "平手";
+                    }
+                    
+                    btnReset.Enabled = true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 回傳本機IP
+        /// </summary>
+        /// <returns></returns>
+        private string MyIP()
+        {
+            string hn = Dns.GetHostName(); //取得本機的主機名稱
+            IPAddress[] ips = Dns.GetHostEntry(hn).AddressList;//取得本機IP陣列(可能多個)
+            foreach (IPAddress ip in ips) //一次(foreach)取一個ip(it)，列舉所有IP
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)//若ip格式是IPv4格式
+                {
+                    return ip.ToString(); //只回傳此格式的IP=>字串
+                }
+            }
+            return ""; //若找不到IP，回傳空字串
+        }        
+        
+        /// <summary>
+        /// 傳送字串給對手
+        /// </summary>
+        /// <param name="str"></param>
+        private void Send(string str)
+        {
+            // 對手的Port
+            int Port = int.Parse(tbTargetPort.Text);
+
+            // 宣告對手的UDP通訊物件
+            UdpClient S = new UdpClient(tbTargetIP.Text, Port);
+            
+            // 轉碼:字串=>B陣列
+            byte[] bStr = Encoding.UTF8.GetBytes(str);
+
+            // 傳送選項給對手
+            S.Send(bStr, bStr.Length); 
+        }
+
+        /// <summary>
+        /// 遊戲規則，並回傳遊戲結果
+        /// </summary>
+        /// <param name="myPick"></param>
+        /// <param name="oppoPick"></param>
+        /// <returns></returns>
+        private string CheckRules(string myPick, string oppoPick)
+        {             
+            // 遊戲結果
+            string gameResult = string.Empty;
+
+            if (myPick == oppoPick)
+            {
+                gameResult = "平手";
+            }
+
+            switch (myPick.ToLower())
+            {
+                // 我方出剪刀
+                case "剪刀":
+
+                    // 對方出叔投
+                    if (oppoPick.ToLower() == "叔投")
+                    {
+                        gameResult = "你輸了";
+                    }
+                    // 對方出布
+                    else if (oppoPick.ToLower() == "布")
+                    {
+                        gameResult = "你贏了";
+                    }
+                    break;
+
+                // 我方出叔投
+                case "叔投":
+
+                    // 對方出布
+                    if (oppoPick.ToLower() == "布")
+                    {
+                        gameResult = "你輸了";
+                    }
+                    // 對方出剪刀
+                    else if (oppoPick.ToLower() == "剪刀")
+                    {
+                        gameResult = "你贏了";
+                    }
+                    break;
+
+                // 我方出布
+                case "布":
+
+                    // 對方出剪刀
+                    if (oppoPick.ToLower() == "剪刀")
+                    {
+                        gameResult = "你輸了";
+                    }
+                    // 對方出叔投
+                    else if (oppoPick.ToLower() == "叔投")
+                    {
+                        gameResult = "你贏了";
+                    }
+                    break;
+            }
+
+            return gameResult;
+        }
+
+        /// <summary>
+        /// 重製遊戲
+        /// </summary>
+        private void ResetGame()
+        {
+            btnReset.Enabled = false;
+            radScissors.Enabled = true;
+            btnSend.Enabled = true;
+            oppPick = null;
+            myPick = null;
+            lblResult.Text = "";
+        }
+
+        #region 所有監聽事件
+
+        /// <summary>
+        /// 啟動監聽按鈕 監聽 - 按下
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnActivate_Click(object sender, EventArgs e)
+        {
+            //忽略其他跨Thread操作的錯誤
+            Control.CheckForIllegalCrossThreadCalls = false;
+
+            //建立監聽的執行緒Thread->Listen
+            Th = new Thread(Listen);
+
+            //啟動監聽Thread
+            Th.Start();
+
+            //已啟動，將此按鈕暫時失效
+            btnActivate.Enabled = false;
+        }
+
+        /// <summary>
+        /// WinForm 監聽 - 載入事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text += "UDP_猜拳遊戲，本機的IP:" + MyIP();
+            tbTargetIP.Text = MyIP();
+        }
+
+        /// <summary>
+        /// WinForm 監聽 - 關閉事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                Th.Abort(); //關閉執行緒
+                U.Close(); //關閉監聽器
+            }
+            catch
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// 傳送按鈕 監聽 - 按下
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            // 將送出按鈕禁用
+            btnSend.Enabled = false;
+
+            // 將重製按鈕啟用
+            btnReset.Enabled = true;
+
+            myPick = string.Empty;
+
+            // 取得所有 radioButton 的值
+            foreach (RadioButton rBtn in this.Controls.OfType<RadioButton>())
+            {                
+                if (rBtn.Checked)
+                {
+                    myPick = rBtn.Text;
+                    break;
+                }
+            }
+
+            // 送出我方資訊
+            Send("oppPick:" + myPick);            
+
+            // 若雙方接送出資料，執行遊戲規則，並將結果同步
+            if (!string.IsNullOrEmpty(myPick) && !string.IsNullOrEmpty(oppPick)){
+
+                // 算出結果
+                var result = CheckRules(myPick, oppPick);
+
+                // 顯示結果
+                lblResult.Text = result;                
+
+                // 將結果一同送出
+                Send("result:" + result);
+            }
+        }        
+
+        /// <summary>
+        /// 重製按鈕 監聽 - 按下
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnReset_Click(object sender, EventArgs e)
+        {            
+            ResetGame();
+        }
+
+        #endregion
+    }
+}
